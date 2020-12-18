3d601ec1f3a80efbb058fdb051c17c0686d89b25
Cole Comstock <cole.d.comstock@gmail.com>
2020-12-18 14:46:38 -0700

    Working on function and for loop

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,32 @@
 // from data.js
 var tableData = data;
 
-// Select the Input Field
+// Select the Input Field for Date Time and the Form Filter Button
 var formDateTime = d3.select('#datetime'); 
 
 var formButton = d3.select('#filter-btn'); 
 
+// Set functions to run on enter and form field change
 formDateTime.on('change', runFilter); 
 formButton.on('click', runFilter); 
 
+// Filter data
+
+// var filteredData = data.filter((object) => object.datetime) 
+// console.log(filteredData)
+
+// Function to Filter and Display results
 function runFilter() {
-    event.preventDefault(); 
+    event.preventDefault();
+
+    var dateInput = formDateTime.event.target.value;
+
+    for (var i = 0; i < data.length; i++) {
+        if (dateInput ==) {
+
+        }
+    }
+
+
+
 }
