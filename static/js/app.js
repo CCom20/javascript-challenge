8df2072560013e0c759bd9f044456c9b634abc7d
Cole Comstock <cole.d.comstock@gmail.com>
2020-12-22 14:24:07 -0700

    Working on multi filters

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,14 @@ var formDateTime = d3.select('#datetime');
 
 var formButton = d3.select('#filter-btn'); 
 
+var cityControl = d3.select('#city');
+
+var stateControl = d3.select('#state');
+
+var countryControl = d3.select('#country');
+
+var shapeControl = d3.select('#shape');
+
 // Set functions to run on enter and form field change
 formDateTime.on('change', runFilter); 
 formDateTime.on('submit', runFilter); 
@@ -17,37 +25,65 @@ function runFilter() {
     
     event.preventDefault();
 
-    var dateInput = formDateTime.property('value');
-
     var tbody = d3.select('tbody');
-
-    var filteredData = data.filter(sighting => sighting.datetime == dateInput)
-    console.log(filteredData)
 
     tbody.html("");
 
-    filteredData.forEach(dataRow => {
-        var tableRow = tbody.append('tr');
+    // SET DATE VARIABLE
+    var dateInput = formDateTime.property('value');
+    
+    // SET CITY VARIABLE, CONVERT TO LOWERCASE FOR SEARCH
+    var cityInput = cityControl.property('value');
+    var cityLower = cityInput.toLowerCase();
 
-        
-    }); 
+    // SET STATE VARIABLE, CONVERT TO LOWERCASE FOR SEARCH 
+    var stateInput = stateControl.property('value');
+    var stateLower = stateInput.toLowerCase();
 
-    // for (var i = 0; i < filteredData.length; i++) {
-        
-    //     var newRow = tbody.append('tr');
+    // SET COUNTY VARIABLE, CONVERT TO LOWERCASE FOR SEARCH
+    var countryInput = countryControl.property('value');
+    var countryLower = countryInput.toLowerCase();
 
-    //     newRow.append('td').text(`${filteredData[i].datetime}`);
-    //     newRow.append('td').text(`${filteredData[i].city}`);
-    //     newRow.append('td').text(`${filteredData[i].state}`);
-    //     newRow.append('td').text(`${filteredData[i].country}`);
-    //     newRow.append('td').text(`${filteredData[i].shape}`);
-    //     newRow.append('td').text(`${filteredData[i].durationMinutes}`);
-    //     newRow.append('td').text(`${filteredData[i].comments}`);
-    // }
+    // SET SHAPED VARIABLE, CONVERT TO LOWERCASE FOR SEARCH
+    var shapeInput = shapeControl.property('value');
+    var shapeLower = shapeInput.toLowerCase();
+
+    // USER DATE INPUT FILTER AND APPEND
+    if (dateInput) {
+        var filteredData = data.filter(sighting => sighting.datetime == dateInput)
+        console.log(filteredData)
+    }
+
+    // USER CITY INPUT FILTER AND APPEND
+    else if (cityInput) {
+        var filteredData = data.filter(sighting => sighting.city == cityLower)
+        console.log(filteredData)
+    }
+
+    // USER STATE INPUT FILTER AND APPEND
+    else if (stateInput) {
+        var filteredData = data.filter(sighting => sighting.state == stateLower)
+        console.log(filteredData)
+    }
+
+    // USER COUNTRY INPUT FILTER AND APPEND
+    else if (countryInput) {
+        var filteredData = data.filter(sighting => sighting.country == countryLower)
+        console.log(filteredData)
+    }
+
+    // USER COUNTRY INPUT FILTER AND APPEND
+    else if (shapeInput) {
+        var filteredData = data.filter(sighting => sighting.shape == shapeLower)
+        console.log(filteredData)
+    }
+
+    filteredData.forEach(function(entry) {
+
+        var newRow = tbody.append('tr');
+
+        Object.keys(entry).forEach(function(key) {
+        newRow.append('td').text(entry[key])
+        })
+    })
 }
-
-var dateControl = d3.select('#datetime');
-var cityControl = d3.select('#city');
-var stateControl = d3.select('#state');
-var countryControl = d3.select('#country');
-var shapeControl = d3.select('#shape');
